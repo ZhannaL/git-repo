5ddeefeb4d7d9531e8d9e9aab38b52e6d9e61706
zhanna <zhannet1814@gmail.com>
2016-08-30 12:37:32 +0300

    commit 2 (2)

--- ConsoleApplication2/ConsoleApplication2/Program.cs
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -7,7 +7,8 @@ using System.Threading.Tasks;
 namespace ConsoleApplication2 {
     class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World");
+            Console.WriteLine("Hello, World!");
+            Console.ReadKey();
         }
     }
 }
