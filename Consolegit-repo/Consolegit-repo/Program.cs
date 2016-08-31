5d027e40c60dd8c81c91956e82c2d1ca6516cf8f
zhanna <zhannet1814@gmail.com>
2016-08-31 11:27:49 +0300

    console git-repo

---
+++ Consolegit-repo/Consolegit-repo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Consolegit_repo {
+    class Program {
+        static void Main(string[] args) {
+            Console.WriteLine("hello, my dear friend!");
+            Console.ReadKey();
+        }
+    }
+}
